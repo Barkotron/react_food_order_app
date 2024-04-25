2ca31a246e20f672b5cc8ff04b75ed7a700e85d6
Jackson Barker <jcrb45@hotmail.com>
2024-04-25 15:46:41 -0500

    completed cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import {CartContextProvider} from "./store/CartContext.jsx";
 
 function App() {
 
   
   return (
-    <>
+    <CartContextProvider>
       <Header></Header>
       <Meals></Meals>
-    </>
+    </CartContextProvider>
   );
 }
 
