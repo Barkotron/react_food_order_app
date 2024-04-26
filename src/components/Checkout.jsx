7987b2bc26e701f9f33b588fe92b68ba823513d4
Jackson Barker <jcrb45@hotmail.com>
2024-04-26 14:50:48 -0500

    added closing for close buttons

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -4,18 +4,28 @@ import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <Modal>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
         <Input label="Full Name" type="text" id="full-name"></Input>
@@ -27,8 +37,10 @@ export default function Checkout() {
         </div>
 
         <p className="modal-actions">
-            <Button type='button' textOnly>Close</Button>
-            <Button>Submit Order</Button>
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
         </p>
       </form>
     </Modal>
